4b690181d75cef2eb264d73d2757382d014f05f8
AlexandraElenaGeorgescu <alestef81@gmail.com>
2021-10-28 00:25:56 +0300

    Add project files.

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Book 
+    {  
+      public string BookName { get;
+            set;
+        }
+       public string Id
+        {
+            get;
+            set;
+        }
+
+         void Display()
+        {
+            Console.WriteLine("bookname: {0}", BookName);
+            
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using VisioForge.Shared.MediaFoundation.OPM;
+
+namespace ConsoleApp1
+{
+    class MeetManager
+    {
+        static void Main(string[] args)
+        {   
+            string ok = "go";
+            Book book = new Book();
+            book.
+            Author a = new Author();
+            while (c.Comparation(ok) != false)
+            { b.Acceptdetails1();
+                a.Acceptdetails2();
+                b.Display();
+                a.Display();
+               ok = Console.ReadLine();
+            }
+        }
+    }
+      public class Author 
+    {    
+        
+
+        public void Display()
+        {
+            Console.WriteLine("authorname: {0}", authorname);
+
+        }
+
+    }
+  class Get
+    {
+        protected string bookname;
+        protected string authorname;
+
+        public void Acceptdetails1()
+        { 
+            bookname = Console.ReadLine();
+
+        }
+        public void Acceptdetails2()
+        {
+            authorname = Console.ReadLine();
+        }
+    }
+}
